26133e51bfb94105921887d2c88c9a205961e24e
Zach Silveira <zackify@gmail.com>
2021-06-26 11:49:20 -0400

    more progress

--- moovite/client.tsx
+++ moovite/client.tsx
@@ -1,7 +1,20 @@
+import React from "react";
 import ReactDOM from "react-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+let Index = React.lazy(() => import("../src/pages/index"));
+let Test = React.lazy(() => import("../src/pages/test"));
 
 ReactDOM.hydrate(
-  //@ts-ignore
-  <Page {...(window._MOOVITE_PROPS_ || {})} />,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/">
+        <Index {...((window as any)._MOOVITE_PROPS_ || {})} />
+      </Route>
+      <Route path="/test">
+        <Test name="wrong" />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("app")
 );
