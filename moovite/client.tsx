42c8bc36ff3f522b22a5c6955bd1c09963f80c00
Zach Silveira <zackify@gmail.com>
2021-06-26 11:49:39 -0400

    revert

--- moovite/client.tsx
+++ moovite/client.tsx
@@ -1,20 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-
-let Index = React.lazy(() => import("../src/pages/index"));
-let Test = React.lazy(() => import("../src/pages/test"));
 
 ReactDOM.hydrate(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/">
-        <Index {...((window as any)._MOOVITE_PROPS_ || {})} />
-      </Route>
-      <Route path="/test">
-        <Test name="wrong" />
-      </Route>
-    </Switch>
-  </BrowserRouter>,
+  //@ts-ignore
+  <Page {...(window._MOOVITE_PROPS_ || {})} />,
   document.getElementById("app")
 );
---
+++ notes.md
@@ -0,0 +1,7 @@
+
+
+## after
+
+want to see more? To make this practical we should be able to do client side routing.
+
+One approach I've thought of, is auto generating a main entry point that references all possible routes
