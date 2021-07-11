29d5b5fa2eb120890e8c5e013edc553bcba15d71
Zach Silveira <zackify@gmail.com>
2021-07-11 12:41:05 -0400

    getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-const Homepage = () => {
-  return <div onClick={() => console.log("hello")}>Hello from moovite</div>;
+export const getServerSideProps = () => {
+  return { message: "Hello from the server!" };
+};
+
+type Props = {
+  message: string;
+};
+const Homepage = ({ message }: Props) => {
+  return <div onClick={() => console.log("hello")}>{message}</div>;
 };
 
 export default Homepage;
